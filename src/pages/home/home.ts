de2659a590c0e383335da1faf1128da3f46e88f4
josedavidb <josedavidb6@gmail.com>
2017-12-18 23:01:04 -0400

    Funcionamiento y configuracion de slider productos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as $ from "jquery";
-//import slick-slider;
+import "slick-carousel";
 
 @Component({
   selector: 'page-home',
@@ -10,44 +10,44 @@ import * as $ from "jquery";
 export class HomePage {
 
 
-  constructor(public navCtrl: NavController) {
-
-
-
-    /*
-
-$('.center').slick({
-  centerMode: true,
-    centerPadding: '250px',
-    slidesToShow: 3,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-  */
+  constructor(public navCtrl: NavController) {   
+    
+   
+  
 
         $(document).on("click",function(){
           console.log("Hola");
         });
 
+  }
+  ionViewDidLoad(){
+    $(".myCarousel").slick();
+    $('.center').slick({
+      centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '0px',
+              slidesToShow: 3
+            }
+          }
+        ]
+      });
   }
 
   
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/slick-carousel/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
